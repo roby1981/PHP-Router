419d4cdacca3545f624b9dfd29752d029d60e4bf
fellfrosch <fellfrosch@fellfrosch-Laptop.fritz.box>
2022-03-22 14:37:17 +0100

    Second commit...

--- index.php
+++ index.php
@@ -1,7 +1,40 @@
 <?php
+$request_url = $_SERVER["REQUEST_URI"];
 
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
 
+$index = function(){
+    echo "Go to index-page.";
+};
+
+$login = function(){
+    echo "Go to login-page.";
+};
+
+$http_404 = function(){
+    echo "404: Not found.";
+};
+
+$routes=[];
+
+$routes['/']=$index;
+$routes['login']=$login;
+
+foreach($routes as $route => $action)
+{
+    $regex="~^$route/?$~i";
+    echo $request_url." - ".$regex."<br>";
+    if(!preg_match($regex, $request_url))
+    {
+        echo "Passt nicht.";
+        continue;
+    }
+    if(!is_callable($action))
+    {
+        return call_user_func($http_404);
+    }
+    return call_user_func($action);
+}
+return call_user_func($http_404);
+
+echo date();
+?>
